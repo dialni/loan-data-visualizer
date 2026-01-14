b4a362b837de51f1d44b8daabac717c660e241e8
dialni <dini@itu.dk>
2026-01-14 07:33:21 +0100

    Converted logic to class for easier handling

--- src/backend/loan_data_visualizer.py
+++ src/backend/loan_data_visualizer.py
@@ -1,54 +1,63 @@
 from models import *
 import reddit_api
 import db_api
-from datetime import datetime, timedelta
+import asyncio
 
 # TODO: Apply exchange rates to currency calculations
 
-def UpdateTimeframeData() -> list[dict]:
-    '''Master function for compiling the data to cache for the API'''
-    # Initialize everything
-    timeframe = []
-    api = reddit_api.APITool()
-    db = db_api.Database()
-    api.Auth()
-    db.CreateTables()
+class TimeframeData():
+    cache = []
     
-    # Get data from Reddit Data API and store in database
-    nextPage = ''
-    for _ in range(3):
-        response = api.GetNewestPosts('borrow', nextPage, 5)
-        db.InsertPostList(response[0])
-        nextPage = response[1]
-    
-    # Validate data
-    NullPosts = db.GetNullActiveLoanRequests()
-    i = 0
-    for id in NullPosts:
-        i += 1
-        db.UpdateActiveOnLoan(id, api.IsPostActive('borrow', id))
-        print(f"NullPost validation: {i} / {NullPosts.__len__()}")
-    
-    # Anonymize data
-    db.AnonymizeData()
-    
-    # Make timeframe
-    for day in range(30):
-        query = db.LoanPaidAndDefaultRate(day) # Experiment in querying data, will probably get removed later.
-        result = {'date': int((datetime.today() - timedelta(day)).timestamp()),
-                  'reqCount': db.LoansRequestedOnDate(day),
-                  'activeCount': db.LoansGivenOnDate(day),
-                  'reqAmount': db.LoanAmountRequestedOnDate(day),
-                  'activeAmount': db.LoanAmountGivenOnDate(day),
-                  'loansPaid': query[0],
-                  'loansUnpaid': query[1]
-                  
-                  }
-        timeframe.append(result)
-    #print(timeframe)
-    db.CloseConnection()
-    return timeframe
-    
+    def __init__(self):
+        '''Create cache on initalization'''
+        self.cache = self.UpdateTimeframeData()
+
+    def GetCache(self) -> list[dict]:
+        return self.cache
+
+    def UpdateTimeframeData(self) -> list[dict]:
+        '''Master function for compiling the data to cache for the API'''
+        # Initialize everything
+        timeframe = []
+        api = reddit_api.APITool()
+        db = db_api.Database()
+        api.Auth()
+        db.CreateTables()
+
+        # Get data from Reddit Data API and store in database
+        nextPage = ''
+        for _ in range(30):
+            response = api.GetNewestPosts('borrow', nextPage, 100)
+            db.InsertPostList(response[0])
+            nextPage = response[1]
+        
+        # Validate data
+        NullPosts = db.GetNullActiveLoanRequests()
+        i = 0
+        for id in NullPosts:
+            i += 1
+            db.UpdateActiveOnLoan(id, api.IsPostActive('borrow', id))
+            print(f"NullPost validation: {i} / {NullPosts.__len__()}")
+        
+        # Anonymize data
+        db.AnonymizeData()
+        
+        # Make timeframe
+        for day in range(30):
+            query = db.LoanPaidAndDefaultRate(day) # Experiment in querying data, will probably get removed later.
+            result = {#'date': int((datetime.today() - timedelta(day)).timestamp()),
+                    'reqCount': db.LoansRequestedOnDate(day),
+                    'activeCount': db.LoansGivenOnDate(day),
+                    'reqAmount': db.LoanAmountRequestedOnDate(day),
+                    'activeAmount': db.LoanAmountGivenOnDate(day),
+                    'loansPaid': query[0],
+                    'loansUnpaid': query[1]
+                    }
+            timeframe.append(result)
+        #print(timeframe)
+        db.CloseConnection()
+        return timeframe
+        
     
 if __name__ == "__main__":
 
@@ -61,4 +70,5 @@ if __name__ == "__main__":
     # - Summary and conclusion
     #  - Hypothetical "total expected ROI for X amount invested"
     
-    print(UpdateTimeframeData())
+    timeframe = TimeframeData()
+    print(timeframe.GetCache())
