20d8a14cae7af2f5ab14ae3d167233c8ea17e5dd
dialni <dini@itu.dk>
2026-01-14 07:32:49 +0100

    refactored api_server to be include proper scheduler

--- src/backend/api_server.py
+++ src/backend/api_server.py
@@ -1,49 +1,33 @@
-from fastapi import FastAPI, Response, BackgroundTasks
+import asyncio
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from loan_data_visualizer import UpdateTimeframeData
-from os import getenv
-from dotenv import load_dotenv
+from loan_data_visualizer import TimeframeData
 
-class ChallengeCode(BaseModel):
-    code: str
+# Local object to maintain timeframe data
+timeframecache = TimeframeData()
 
-if not load_dotenv('.env'):
-    raise SystemExit("api_server.py could not open .env, exiting...")
+# Self-contained scheduler to clear and update timeframe data every day.
+# Responses from this server are delayed during execution, but that is acceptable for this service.
+async def UpdateCacheScheduler():
+    global timeframecache
+    while True:
+        await asyncio.sleep(60) # 1 day delay
+        print("Updating cache...")
+        timeframecache.UpdateTimeframeData()
 
-if getenv("API_SERVER_CHALLENGECODE") == None:
-    raise SystemExit("API_SERVER_CHALLENGECODE not set, exiting...")
+# Creates scheduler task on app start
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting UpdateCacheScheduler...")
+    asyncio.create_task(UpdateCacheScheduler())
+    yield
 
-app = FastAPI(docs_url=None, redoc_url=None)
+app = FastAPI(lifespan=lifespan, docs_url=None, redoc_url=None)
 
 # Public endpoint for getting data in cache
 @app.get("/get-timeframe")
 def GetTimeframe():
-    try: app.state.timeframeCache != None
-    except AttributeError: return JSONResponse(jsonable_encoder([]))    
-    return JSONResponse(jsonable_encoder(app.state.timeframeCache))
-
-# Private method for starting data caching
-def StartUpdateTimeframeJob():
-    '''Background job to create a new Timeframe'''
-    print(f"Lock state: {app.state.timeframeLock}")
-    app.state.timeframeCache = UpdateTimeframeData()
-    app.state.timeframeLock = False
-    print(f"Lock state: {app.state.timeframeLock}")
-
-# Public endpoint for starting the data caching, if the correct password is provided.
-# This is intended to be used with a cronjob to start at certain times of the day,
-@app.post("/update-timeframe")
-def UpdateTimeframe(challengeCode: ChallengeCode, background_tasks: BackgroundTasks):
-    try: app.state.timeframeLock != None
-    except AttributeError:
-        app.state.timeframeLock = False
-        app.state.timeframeCache = []
-    if challengeCode.code != getenv("API_SERVER_CHALLENGECODE"):
-        return Response("403\n", 403)
-    if app.state.timeframeLock == True:
-        return Response("409\n", 409)
-    app.state.timeframeLock = True
-    background_tasks.add_task(StartUpdateTimeframeJob)
-    return Response("202\n", 202)
+    global timeframecache
+    return JSONResponse(jsonable_encoder(timeframecache.GetCache()))
